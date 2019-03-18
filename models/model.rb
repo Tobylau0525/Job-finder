b4318e1a5e225cd1034424e714492ed98b157e70
Toby lau <yatchol6214@hstat.org>
2019-03-18 01:24:41 +0000

    push for deploying on Heroku

--- models/model.rb
+++ models/model.rb
@@ -1,6 +1,6 @@
 require 'careerjet/api_client'
 require 'pp' 
- 
+require 'dotenv/load'
  
 #  cj_api_client = Careerjet::APIClient.new(:locale=> :en_GB)
 
@@ -19,7 +19,7 @@ require 'pp'
       pp api.search(
       :keywords   => keyword,
       :location   => location,
-      :affid      => '1f8f344630cdb44534351ce3cd6c2d0c',
+      :affid      => ENV['API_KEY'],
       :user_ip    => '11.22.33.44',
       :user_agent => 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
       :url        => 'http://www.example.com/jobsearch?q=ruby&l=london'
