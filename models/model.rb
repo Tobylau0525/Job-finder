e3ca21763c5e7d86c4ce7e827b57a069bf0856ec
yatchol6214 <yatchol6214@hstat.org>
2019-03-25 05:59:27 -0400

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -13,9 +13,9 @@ require 'dotenv/load'
 #      :url        => 'http://www.example.com/jobsearch?q=ruby&l=london'
 #  ).jobs.first
  
-  def get_job_offer(api,keyword,location)
+  def get_job_offer(api,keyword,location)#using user input to search to Job offer 
       
-    begin  
+     
       api.search(
       :keywords   => keyword,
       :location   => location,
@@ -23,52 +23,6 @@ require 'dotenv/load'
       :user_ip    => '11.22.33.44',
       :user_agent => 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
       :url        => 'http://www.example.com/jobsearch?q=ruby&l=london'
-      ).jobs.first
-    rescue
-      get_job_offer = ["Sorry, no Job offer was found"]
-    end
-     
-  
-  
+      ).jobs.first 
      
 end     
-
-
-# class Job
-#     attr_accessor :api,:language,:location
-   
-#     def initialize(cj_api_client,programming_language,location)
-#         @api=cj_api_client
-#         @language=programming_language
-#         @location=location
-      
-#     end  
-   
-#     def get_job_offer
-      
-   
-#         pp api.search(
-#             :keywords   => language,
-#             :location   => location,
-#             :affid      => '1f8f344630cdb44534351ce3cd6c2d0c',
-#             :user_ip    => '11.22.33.44',
-#             :user_agent => 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
-#             :url        => 'http://www.example.com/jobsearch?q=ruby&l=london'
-#         ).jobs.first
- 
- 
-     
-#end  
- 
-# end    
-
-
-
-
-
-
-# get_job_offer(cj_api_client,'ruby','NY')
-
-# ruby=Job.new(cj_api_client,'ruby','NY')
-# puts ruby.get_job_offer
-
